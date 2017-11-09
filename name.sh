07cbfd54eeb83c8dc3a9a08bde2a0941a3a09a83
Cosenza <ccosenza281@studentubuntu14.ecc.iwcc.edu>
2017-11-09 16:15:31 -0600

    First bash scripts.

---
+++ name.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+name="Cam Cosenza"
+echo "Hello, $name"
+echo " "
+read -p "How old are you? " age
+echo " "
+echo "$name is $age years old and you are $1 $2."
+echo " "
+read -p "What is you favorite color? " color
+echo " "
+echo "$name favorite color is $color." 
